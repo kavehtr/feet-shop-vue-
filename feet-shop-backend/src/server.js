6f57b09ce16dbff61f8aa84fe9464443b6a4abac
Neptune Tech <93869615+kavehtr@users.noreply.github.com>
2023-06-11 09:59:19 +0330

    update

--- feet-shop-backend/src/server.js
+++ feet-shop-backend/src/server.js
@@ -6,7 +6,7 @@ const app = express();
 
 app.get('/ProductPage', (req, res) => {
     res.json(products);
-})
+});
 
 app.get('/ShoppingCartPage', (req, res) => {
     res.json(cartProducts);
@@ -16,8 +16,12 @@ app.get('/ProductPage/:ProductId', (req, res) => {
     const productId = req.params.ProductId;
     const product = products.find(product => product.id === productId);
     res.json(product);
+});
+
+app.post('/shoppingCartList', (req, res) => {
+    
 })
 
 app.listen(8000, () => {
     console.log('Server Is Listening on Port 8000');
-})
+});
